3fc1381e20b61bdd0dcf7a5197993e42e0e3ef19
Christian Hergert <chergert@redhat.com>
2022-06-19 16:00:29 -0700

    main: add GTK and GtkSourceView version information
    This can help us a bit when tracking down issues with libraries.

--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@
 #include "config.h"
 
 #include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include <gtksourceview/gtksource.h>
 
 #include "build-ident.h"
 #include "editor-application-private.h"
@@ -47,6 +49,12 @@ check_early_opts (int        *argc,
   if (version)
     {
       g_printerr ("%s %s (%s)\n", PACKAGE_NAME, PACKAGE_VERSION, EDITOR_BUILD_IDENTIFIER);
+      g_printerr ("            GTK: %d.%d.%d (Compiled against %d.%d.%d)\n",
+                  gtk_get_major_version (), gtk_get_minor_version (), gtk_get_micro_version (),
+                  GTK_MAJOR_VERSION, GTK_MINOR_VERSION, GTK_MICRO_VERSION);
+      g_printerr ("  GtkSourceView: %d.%d.%d (Compiled against %d.%d.%d)\n",
+                  gtk_source_get_major_version (), gtk_source_get_minor_version (), gtk_source_get_micro_version (),
+                  GTK_SOURCE_MAJOR_VERSION, GTK_SOURCE_MINOR_VERSION, GTK_SOURCE_MICRO_VERSION);
       exit (EXIT_SUCCESS);
     }
 }
