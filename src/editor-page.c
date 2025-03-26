8ee92d833aea7113118071880a0099955cdc82ee
Christian Hergert <chergert@redhat.com>
2025-03-26 11:10:43 -0700

    page: fix resource leak

--- src/editor-page.c
+++ src/editor-page.c
@@ -632,14 +632,15 @@ goto_line_entry_insert_text_cb (EditorPage *self,
                                 int        *position,
                                 GtkText    *entry)
 {
+  g_autoptr(GString) str = NULL;
   const char *text;
-  GString *str;
 
   g_assert (EDITOR_IS_PAGE (self));
   g_assert (position != NULL);
   g_assert (GTK_IS_TEXT (entry));
 
   text = gtk_editable_get_text (GTK_EDITABLE (entry));
+
   str = g_string_new (text);
   g_string_insert_len (str, *position, new_text, length);
 
@@ -651,7 +652,7 @@ goto_line_entry_insert_text_cb (EditorPage *self,
         continue;
 
       g_signal_stop_emission_by_name (entry, "insert-text");
-      return;
+      break;
     }
 }
 
