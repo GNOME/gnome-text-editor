GNOME/gnome-text-editor

commit 14c7fb950045d24096c06b85227c4cea574b718b

page: Allow move-next and previous when search bar is closed
Fixes: #854

src/editor-page.c
@@ -1545,12 +1545,23 @@ editor_page_get_busy (EditorPage *self)
   return editor_document_get_busy (self->document);
 }
 
+static gboolean
+_editor_page_get_search_visible (EditorPage *self)
+{
+  g_return_val_if_fail (EDITOR_IS_PAGE (self), FALSE);
+
+  return gtk_revealer_get_child_revealed (GTK_REVEALER (self->search_revealer));
+}
+
 static void
 _editor_page_set_search_visible (EditorPage          *self,
                                  gboolean             search_visible,
                                  EditorSearchBarMode  mode)
 {
   g_return_if_fail (EDITOR_IS_PAGE (self));
+
+  if (search_visible == _editor_page_get_search_visible (self))
+    return;
 
   if (search_visible)
     {
@@ -1610,6 +1621,8 @@ _editor_page_move_next_search (EditorPage *self,
 {
   g_return_if_fail (EDITOR_IS_PAGE (self));
 
+  _editor_page_set_search_visible (self, TRUE, EDITOR_SEARCH_BAR_MODE_SEARCH);
+
   _editor_search_bar_move_next (self->search_bar, hide_after_move);
 }
 
@@ -1618,6 +1631,8 @@ _editor_page_move_previous_search (EditorPage *self,
                                    gboolean    hide_after_move)
 {
   g_return_if_fail (EDITOR_IS_PAGE (self));
+
+  _editor_page_set_search_visible (self, TRUE, EDITOR_SEARCH_BAR_MODE_SEARCH);
 
   _editor_search_bar_move_previous (self->search_bar, hide_after_move);
 }

commit 45bd54c5219aede5277fe5d3015bac4a770f7a25

Fix search/replace switching and re-focus search bar with shortcuts
These fixes address some regressions from 14c7fb95

Thanks to:  @skeller

Fixes: #867, #868

src/editor-page.c
@@ -1560,7 +1560,8 @@ _editor_page_set_search_visible (EditorPage          *self,
 {
   g_return_if_fail (EDITOR_IS_PAGE (self));
 
-  if (search_visible == _editor_page_get_search_visible (self))
+  if (search_visible == _editor_page_get_search_visible (self) &&
+      mode == _editor_search_bar_get_mode (self->search_bar))
     return;
 
   if (search_visible)
@@ -1586,6 +1587,7 @@ _editor_page_begin_search (EditorPage *self)
   g_return_if_fail (EDITOR_IS_PAGE (self));
 
   _editor_page_set_search_visible (self, TRUE, EDITOR_SEARCH_BAR_MODE_SEARCH);
+  _editor_search_bar_grab_focus (self->search_bar);
 }
 
 void
@@ -1594,6 +1596,7 @@ _editor_page_begin_replace (EditorPage *self)
   g_return_if_fail (EDITOR_IS_PAGE (self));
 
   _editor_page_set_search_visible (self, TRUE, EDITOR_SEARCH_BAR_MODE_REPLACE);
+  _editor_search_bar_grab_focus (self->search_bar);
 }
 
 void
@@ -1621,7 +1624,7 @@ _editor_page_move_next_search (EditorPage *self,
 {
   g_return_if_fail (EDITOR_IS_PAGE (self));
 
-  _editor_page_set_search_visible (self, TRUE, EDITOR_SEARCH_BAR_MODE_SEARCH);
+  _editor_page_set_search_visible (self, TRUE, _editor_search_bar_get_mode (self->search_bar));
 
   _editor_search_bar_move_next (self->search_bar, hide_after_move);
 }
@@ -1632,7 +1635,7 @@ _editor_page_move_previous_search (EditorPage *self,
 {
   g_return_if_fail (EDITOR_IS_PAGE (self));
 
-  _editor_page_set_search_visible (self, TRUE, EDITOR_SEARCH_BAR_MODE_SEARCH);
+  _editor_page_set_search_visible (self, TRUE, _editor_search_bar_get_mode (self->search_bar));
 
   _editor_search_bar_move_previous (self->search_bar, hide_after_move);
 }

src/editor-search-bar-private.h
@@ -69,6 +69,8 @@ struct _EditorSearchBar
 void     _editor_search_bar_attach              (EditorSearchBar     *self,
                                                  EditorDocument      *document);
 void     _editor_search_bar_detach              (EditorSearchBar     *self);
+EditorSearchBarMode
+         _editor_search_bar_get_mode            (EditorSearchBar     *self);
 void     _editor_search_bar_set_mode            (EditorSearchBar     *self,
                                                  EditorSearchBarMode  mode);
 void     _editor_search_bar_move_next           (EditorSearchBar     *self,

src/editor-search-bar.c
@@ -548,10 +548,7 @@ editor_search_bar_get_property (GObject    *object,
   switch (prop_id)
     {
     case PROP_MODE:
-      g_value_set_enum (value,
-                        gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (self->replace_mode_button)) ?
-                        EDITOR_SEARCH_BAR_MODE_REPLACE :
-                        EDITOR_SEARCH_BAR_MODE_SEARCH);
+      g_value_set_enum (value, _editor_search_bar_get_mode (self));
       break;
 
     case PROP_CAN_MOVE:
@@ -735,6 +732,16 @@ editor_search_bar_init (EditorSearchBar *self)
   g_action_map_add_action (G_ACTION_MAP (group), G_ACTION (word_action));
 
   gtk_widget_insert_action_group (GTK_WIDGET (self), "search-options", G_ACTION_GROUP (group));
+}
+
+EditorSearchBarMode
+_editor_search_bar_get_mode (EditorSearchBar *self)
+{
+  g_return_val_if_fail (EDITOR_IS_SEARCH_BAR (self), EDITOR_SEARCH_BAR_MODE_SEARCH);
+
+  return gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (self->replace_mode_button)) ?
+    EDITOR_SEARCH_BAR_MODE_REPLACE :
+    EDITOR_SEARCH_BAR_MODE_SEARCH;
 }
 
 void